dce5481522c54c1318274005b0bee4a0ac1747a9
kieran coito <kierancotio@gmail.com>
2020-03-03 14:57:13 -0800

    moved item generation to the customer

--- customer.py
+++ customer.py
@@ -44,3 +44,25 @@ class customer:
         self.IPM = IPM
         self.number_of_items = number_of_items
         self.chitchatness = chitchatness
+
+    def number_of_items_per_customer(self):
+        ''' calculates distribution of of items
+        '''
+        # -(0 - 15)(uniform) = 40%
+        # -(15-30) (uniform)  = 40%
+        # -(30-70)(normal->split in the middle) = 15%
+        # -(70-200)(log distribution(major between 70-100)) = 15%
+
+        # Number for selection
+        random_selector = np.random.rand()
+        number_of_items = 0
+        if (random_selector < 0.8):
+            # for 0 - 30
+            number_of_items = int(np.random.rand() * 30)
+        elif (random_selector < 0.95):
+            # for 30 - 70
+            number_of_items = int(np.random.normal(20, 8.9) + 30)
+        else:
+            # for 70-200
+            number_of_items = int(np.random.lognormal(3, 0.63) + 70)
+        return number_of_items
