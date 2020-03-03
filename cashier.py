b5bb2c254d9eb56d67263b3a765a9156e808201c
kieran coito <kierancotio@gmail.com>
2020-03-03 14:33:37 -0800

    fixdro

--- cashier.py
+++ cashier.py
@@ -32,7 +32,7 @@ class cashier:
     # Implemented
     self_checkout = False
 
-    toal_number_if_items_by_customers_in_systems = 0
+    total_number_if_items_by_customers_in_systems = 0
     #forgetfullness 
 
     def __init__(self, IPM, chitchatter, maintenance_cost, self_checkout=False):
@@ -48,8 +48,8 @@ class cashier:
     def add_customer_to_queue(self, customer):
         """ adds a customer to this cashier's queue
         """
-        toal_number_if_items_by_customers_in_systems = \
-            toal_number_if_items_by_customers_in_systems\ 
+        self.total_number_if_items_by_customers_in_systems = \
+            self.total_number_if_items_by_customers_in_systems\
             + customer.number_of_items
         self.cashier_queue.insert(0, customer)
         self.complete_queue.append(customer)
