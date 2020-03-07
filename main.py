c67497c701fdace98689a3679de7d192fd33907d
Pedro Goncalves Mokarzel <pgmoka@live.com>
2020-03-07 11:57:30 -0800

    Sets up methods for analysis

--- main.py
+++ main.py
@@ -9,19 +9,7 @@ import matplotlib.pyplot as plt
 # Create simulation code
 import variables as v
 from model import model
-
-# =======================================================================
-# =============================== Methods ===============================
-# =======================================================================
-
-def type_of_cashier_comparison():
-    self_check_model = model("equal", 20, 4, 0,cashier_IPM_p_influence=0.1, customer_IPM_p_influence=0.2)
-    list_of_customers_out_of_system, \
-        list_of_customers_in_line, \
-        list_of_customers_on_cashier_queue,\
-        list_of_items_checked,\
-        cost_for_maintenance\
-            = self_check_model.execute_simulation(number_of_epochs_for_simulation, show=True, showAnim=True)
+from analysis_utils import *
 
 #=======================================================================
 #============================= Exectuion ===============================
