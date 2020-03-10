c7a78457d0ff1218cf1a273cee8d4a367fca168c
Pedro Goncalves Mokarzel <pgmoka@live.com>
2020-03-10 13:52:34 -0700

    adds pictures and customer entering analysis

--- analysis_utils.py
+++ analysis_utils.py
@@ -17,7 +17,7 @@ Notes:
 =======================================================================
 '''
 # =======================================================================
-# ============================= Imports==================================
+# ============================= Imports =================================
 # =======================================================================
 
 # Python modules
@@ -749,8 +749,25 @@ def sensitivity_chitchatness_analysis_for_all_lines(number_of_epochs_for_simulat
 def sensitivity_chitchatness_analysis(number_of_epochs_for_simulation, model_name, number_of_av_simulations,
                                       sensitivity_range, \
                                       number_of_people=150):
-    '''
-    '''
+    """ Sensitivity related to the number of cashier chitchatness, test for specific model type
+
+    Precondition:
+    - number_of_epochs_for_simulation: number of epochs used to run the simulation
+    - model_name,number_of_av_simulations: name of the model used during the test
+        - Chosen between "equal", "cashier", and "customer"
+    - sensitivity_range: range of sensitivity to be tested
+    - number_of_people: Number of people to go into the system begin. Default = 100
+
+    Postcondition:
+    - sens_analysis_list_of_customers_out_of_system: an int list of the average total number of
+    customers out of system at the end of the simulation
+    - sens_analysis_list_of_customers_in_line: an int list of the average customers in line
+     at the end of the simulation
+    - sens_analysis_list_of_customers_on_cashier_queue: an int list of the average customers on 
+    queues at the end of the simulation
+    - sens_analysis_list_of_customer_items_checked: an int list of the average total items checked
+     at the end of the simulation
+    """
 
     # Sets up analysis list
     sens_analysis_list_of_customers_out_of_system = []
@@ -771,6 +788,170 @@ def sensitivity_chitchatness_analysis(number_of_epochs_for_simulation, model_nam
         for i in range(number_of_av_simulations):
             self_check_model = model(model_name, number_of_people, 10, 0, cashier_IPM_p_influence=0,
                                      chitchatness_influence=(item_iterator / 100))
+
+            list_of_customers_out_of_system, \
+            list_of_customers_in_line, \
+            list_of_customers_on_cashier_queue, \
+            list_of_items_checked, \
+            cost_for_maintenance \
+                = self_check_model.execute_simulation(number_of_epochs_for_simulation, show=False, showAnim=False)
+
+            # Adds to average
+            sens_customers_out_of_system_average = sens_customers_out_of_system_average \
+                                                   + list_of_customers_out_of_system[-1]
+
+            sens_customers_in_line_average = sens_customers_in_line_average \
+                                             + list_of_customers_in_line[-1]
+
+            sens_customers_on_cashier_queue_average = sens_customers_on_cashier_queue_average \
+                                                      + list_of_customers_on_cashier_queue[-1]
+
+            sens_items_customer_checked_average = sens_items_customer_checked_average \
+                                                  + list_of_items_checked[0] - list_of_items_checked[-1]
+        # Calulates average
+        sens_customers_out_of_system_average = sens_customers_out_of_system_average / number_of_av_simulations
+        sens_customers_in_line_average = sens_customers_in_line_average / number_of_av_simulations
+        sens_customers_on_cashier_queue_average = sens_customers_on_cashier_queue_average / number_of_av_simulations
+        sens_items_customer_checked_average = sens_items_customer_checked_average / number_of_av_simulations
+
+        sens_customers_out_of_system_average = sens_customers_out_of_system_average / number_of_people
+        sens_customers_in_line_average = sens_customers_in_line_average / number_of_people
+        sens_customers_on_cashier_queue_average = sens_customers_on_cashier_queue_average / number_of_people
+
+        # Appends to average list
+        sens_analysis_list_of_customers_out_of_system.append(sens_customers_out_of_system_average)
+        sens_analysis_list_of_customers_in_line.append(sens_customers_in_line_average)
+        sens_analysis_list_of_customers_on_cashier_queue.append(sens_customers_on_cashier_queue_average)
+        sens_analysis_list_of_customer_items_checked.append(sens_items_customer_checked_average)
+
+    return sens_analysis_list_of_customers_out_of_system, sens_analysis_list_of_customers_in_line, \
+           sens_analysis_list_of_customers_on_cashier_queue, sens_analysis_list_of_customer_items_checked
+
+
+def sensitivity_customer_number_analysis_for_all_lines(number_of_epochs_for_simulation, number_of_av_simulations=200, \
+                                                    sensitivity_range=80, number_of_people=100):
+    """ Sensitivity related number of customers in the system, test for all models type
+
+    Precondition:
+    - number_of_epochs_for_simulation: number of epochs each simulation is going to
+    be ran for
+    - number_of_av_simulations: Numbe of simulations that are going to be ran to
+    calculate the average. Default = 200
+    - sensitivity_range: Range for the sensitivity test. Default = 10
+    - number_of_people: number of people to enter the system. Default = 100
+
+    Postcondition:
+    - 4 png images saved to a file called "analysis_images"
+        - x-axis has the sensitivity test
+        - y-axis has the quantity related to the test
+        - Related to the outputs printed at the test.
+    """
+
+    # Numbers for simulation
+    x_axis = np.arange(sensitivity_range)
+
+    # Runs simulations with the different types of line:
+    sens_analysis_list_of_customers_out_of_system_CUSTOMER, sens_analysis_list_of_customers_in_line_CUSTOMER, \
+    sens_analysis_list_of_customers_on_cashier_queue_CUSTOMER, sens_analysis_list_of_customer_items_checked_CUSTOMER = \
+        sensitivity_number_of_customers_analysis(number_of_epochs_for_simulation, "customer", number_of_av_simulations,
+                                          sensitivity_range, \
+                                          number_of_people=number_of_people)
+
+    sens_analysis_list_of_customers_out_of_system_CASHIER, sens_analysis_list_of_customers_in_line_CASHIER, \
+    sens_analysis_list_of_customers_on_cashier_queue_CASHIER, sens_analysis_list_of_customer_items_checked_CASHIER = \
+        sensitivity_number_of_customers_analysis(number_of_epochs_for_simulation, "cashier", number_of_av_simulations,
+                                          sensitivity_range, \
+                                          number_of_people=number_of_people)
+
+    sens_analysis_list_of_customers_out_of_system_EQUAL, sens_analysis_list_of_customers_in_line_EQUAL, \
+    sens_analysis_list_of_customers_on_cashier_queue_EQUAL, sens_analysis_list_of_customer_items_checked_EQUAL = \
+        sensitivity_number_of_customers_analysis(number_of_epochs_for_simulation, "equal", number_of_av_simulations,
+                                          sensitivity_range, \
+                                          number_of_people=number_of_people)
+
+    # Prints number of customers out of system
+    plt.figure(14)
+    plt.clf()
+    plt.title("Sensitivity Analysis for Customers Out of the System")
+    plt.xlabel("Increment in the Number of Customers")
+    plt.ylabel("Percentage of Mean of Customers Out of System at %d Epochs" % number_of_epochs_for_simulation)
+    plt.plot(x_axis, sens_analysis_list_of_customers_out_of_system_CUSTOMER, 'g', \
+             x_axis, sens_analysis_list_of_customers_out_of_system_CASHIER, 'b', \
+             x_axis, sens_analysis_list_of_customers_out_of_system_EQUAL, 'r')
+    plt.savefig("analysis_images\\number_of_customers_sens_analysis_list_of_customers_out_of_system_%d.png" % number_of_people)
+
+    plt.figure(15)
+    plt.clf()
+    plt.title("Sensitivity Analysis for People in Line Average of %d" % number_of_av_simulations)
+    plt.xlabel("Increment in the Number of Customers")
+    plt.ylabel("Percentage of Mean of Customers in Line at %d Epochs" % number_of_epochs_for_simulation)
+    plt.plot(x_axis, sens_analysis_list_of_customers_in_line_CUSTOMER, 'g', \
+             x_axis, sens_analysis_list_of_customers_in_line_CASHIER, 'b', \
+             x_axis, sens_analysis_list_of_customers_in_line_EQUAL, 'r')
+    plt.savefig("analysis_images\\number_of_customers_sens_analysis_list_of_customers_in_line_%d.png" % number_of_people)
+
+    plt.figure(16)
+    plt.clf()
+    plt.title("Sensitivity Customers in Queue Average of %d" % number_of_av_simulations)
+    plt.xlabel("Increment in the Number of Customers")
+    plt.ylabel("Percentage of Mean of Cashiers on Queue in the End of Simulation at %d Epochs" % number_of_epochs_for_simulation)
+    plt.plot(x_axis, sens_analysis_list_of_customers_on_cashier_queue_CUSTOMER, 'g', \
+             x_axis, sens_analysis_list_of_customers_on_cashier_queue_CASHIER, 'b', \
+             x_axis, sens_analysis_list_of_customers_on_cashier_queue_EQUAL, 'r')
+    plt.savefig("analysis_images\\number_of_customers_sens_analysis_list_of_customers_on_cashier_queue_%d.png" % number_of_people)
+
+    plt.figure(17)
+    plt.clf()
+    plt.title("Sensitivity Analysis for Items Checked Average of %d" % number_of_av_simulations)
+    plt.xlabel("Increment in the Number of Customers")
+    plt.ylabel("Mean of Items checkedout at %d Epochs" % number_of_epochs_for_simulation)
+    plt.plot(x_axis, sens_analysis_list_of_customer_items_checked_CUSTOMER, 'g', \
+             x_axis, sens_analysis_list_of_customer_items_checked_CASHIER, 'b', \
+             x_axis, sens_analysis_list_of_customer_items_checked_EQUAL, 'r')
+    plt.savefig("analysis_images\\number_of_customers_sens_analysis_list_of_items_checked_%d.png" % number_of_people)
+
+
+def sensitivity_number_of_customers_analysis(number_of_epochs_for_simulation, model_name, number_of_av_simulations,
+                                    sensitivity_range, \
+                                    number_of_people=150):
+    """ Sensitivity related to the number of customers in the system, test for specific model type
+
+    Precondition:
+    - number_of_epochs_for_simulation: number of epochs used to run the simulation
+    - model_name,number_of_av_simulations: name of the model used during the test
+        - Chosen between "equal", "cashier", and "customer"
+    - sensitivity_range: range of sensitivity to be tested
+    - number_of_people: Number of people to go into the system begin. Default = 100
+
+    Postcondition:
+    - sens_analysis_list_of_customers_out_of_system: an int list of the average total number of
+    customers out of system at the end of the simulation
+    - sens_analysis_list_of_customers_in_line: an int list of the average customers in line
+     at the end of the simulation
+    - sens_analysis_list_of_customers_on_cashier_queue: an int list of the average customers on 
+    queues at the end of the simulation
+    - sens_analysis_list_of_customer_items_checked: an int list of the average total items checked
+     at the end of the simulation
+    """
+
+    # Sets up analysis list
+    sens_analysis_list_of_customers_out_of_system = []
+    sens_analysis_list_of_customers_in_line = []
+    sens_analysis_list_of_customers_on_cashier_queue = []
+    sens_analysis_list_of_customer_items_checked = []
+
+    # Loops between sensitivities
+    # 0.8 -> 0.95
+    for item_iterator in range(sensitivity_range):
+
+        sens_customers_out_of_system_average = 0
+        sens_customers_in_line_average = 0
+        sens_customers_on_cashier_queue_average = 0
+        sens_items_customer_checked_average = 0
+
+        # Number of simulations used for average of simulation
+        for i in range(number_of_av_simulations):
+            self_check_model = model(model_name, number_of_people+(item_iterator), 10, 0)
 
             list_of_customers_out_of_system, \
             list_of_customers_in_line, \
